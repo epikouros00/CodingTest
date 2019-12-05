26ec6d379a14d0856950bfdf0371bace5ff92cd6
epikouros00 <epikouros00@gmail.com>
2019-12-05 13:14:57 +0900

    NQueen BackTracking algorithm added

--- NQueen/nQueen.c
+++ NQueen/nQueen.c
@@ -2,25 +2,25 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 int answer = 0;
 int sizeN = 0;
 
-bool isPromising(int* arr, int n)
+bool isPromising(int* arr, int pos)
 {
-    for(int i = 0; i < n; ++i)
+    for(int i = 0; i < pos; ++i)
     {
-        if(arr[i] == arr[n]) return false;
-        if((arr[i] - arr[n]) ==  (i - n)) return false;
-        if((arr[i] - arr[n]) ==  (n - i)) return false;
+        if(arr[i] == arr[pos]) return false;
+        if((arr[i] - arr[pos]) ==  (i - pos)) return false;
+        if((arr[i] - arr[pos]) ==  (pos - i)) return false;
     }
     return true;
 }
 
-void enumerate(int* arr, int n)
+void enumerate(int* arr, int pos)
 {
-    
-    if(n == sizeN)
+    if(pos == sizeN)
     {
         answer++;
     }
@@ -28,10 +28,10 @@ void enumerate(int* arr, int n)
     {
         for(int i = 0; i < sizeN; ++i)
         {
-            arr[n] = i;
-            if(isPromising(arr, n))
+            arr[pos] = i;
+            if(isPromising(arr, pos))
             {
-                enumerate(arr, n+1);
+                enumerate(arr, pos+1);
             }
         }
     }
@@ -47,13 +47,60 @@ int solution(int n) {
     return answer;
 }
 
+
+/////////////////////////////////////////////////////////////
+int size, ans, col[15], inc[30], dec[30];
+void sol(int r)
+{
+    if(r == size)
+    {
+        ans++;
+    }
+    else
+    {
+        for(int i=0; i<size; ++i)
+        {
+            if(!col[i] && !inc[r+i] && !dec[size+(r-i)])
+            {
+                col[i] = inc[r+i] = dec[size+(r-i)] = 1;    // inc[r+i]->'/'direction, dec[size+r-i]->'\'direction
+                sol(r+1);
+                col[i] = inc[r+i] = dec[size+(r-i)] = 0;
+            }
+        }
+    }
+}
+
+int solution2(int n)
+{
+    size = n;
+    ans = 0;
+    memset(col, 0, sizeof(int)*15);
+    memset(inc, 0, sizeof(int)*30);
+    memset(dec, 0, sizeof(int)*30);
+    sol(0);
+    return ans;
+}
+    
+////////////////////////////////////////////////////////////
 int main()
 {
     printf("Input size of chess table (n x n): ");
     int num;
     scanf("%d", &num);
 
-    printf("Number of truce cases: %d \n", solution(num));
+    time_t start_, end_;
+    int result = 0;
+//    start_ = clock();
+//    result = solution(num);
+//    end_ = clock();
+//    printf("%lld %lld \n",start_, end_);
+//    printf("Table size: %2d -> Number of truce cases: %7d // Time complexity: %lld(msec)\n", num, result, (clock()-start_));
+
+    start_ = clock();
+    result = solution2(num);
+    end_ = clock();
+    printf("%lld %lld \n",start_, end_);
+    printf("Table size: %2d -> Number of truce cases: %7d // Time complexity: %lld(msec)\n", num, result, (clock()-start_));
 
     return 1;
 }
